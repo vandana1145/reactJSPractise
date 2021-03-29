cb02cb9341b8a75e59535e22e65f92101774dc87
Vandana Kumari <vandana.kumari@semapp.de>
2021-03-29 20:49:07 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from "react";
+import "./App.css";
+// import Student from "./Student";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// // class App extends Component{
+// //   render(){
+// //     return (
+// //       // <Fragment>
+// //       //     <h1>Hello World!!!</h1>
+// //       //     <h2>Coding in ReactJS.</h2>
+// //       // </Fragment>
+// //       <>
+// //       <h1>Hello World!!!</h1>
+// //       <h2>Coding in ReactJS.</h2>
+// //       </>
+// //     );
+// //   }
+// // }
 
-export default App;
+// const App = () => {
+//   return (
+//     <div>
+//       <Student name="C++"/>
+//       <Student name="Java"/>
+//       <Student name="Python"/>
+//       <Student name="JavaScript"/>
+//     </div>
+//   )
+// }
+
+// Creating a simple JSX
+//const el = <h1>Hello Programmers!</h1>
+//const el = <h1>{10+20}</h1>
+//const name = "Vandana";
+//const el = <h1>Hello {name}</h1>
+
+// function rendering
+// function show(name) {
+//   return name;
+// }
+
+// const el = <h1>Hello {show("React")}</h1>
+
+// Property
+// let user = {
+//   fname: "Programming",
+//   lname: "Buddies"
+// };
+//const el = <h1>Hello {user.fname} {user.lname}</h1>
+
+const el = <h1 className="bg">Hello Programming!!!</h1>
+
+export default el;
---
+++ src/Student.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+
+// // Functional component
+// const Student = (props) => {
+//     return <h1>Hello {props.name}!!</h1>
+// }
+
+// // Class based component
+// class Student extends Component {
+//     render() {
+//         return(
+//             <h1>Hello {this.props.name}</h1>
+//         );
+//     }
+// }
+
+const Student = (props) => {
+    return <h1>Hello {props.name}!</h1>
+};
+
+export default Student;
